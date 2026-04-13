b765a2aacda3c991bfb1bd3aa930274c24317ba7
engsiam <siam.softwareengineer.com>
2026-04-13 12:19:33 +0600

    fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-blue-950">
+      <body className="min-h-full flex flex-col">
         <Toaster 
           position="top-right"
           toastOptions={{
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -19,7 +19,7 @@ export function Footer() {
           </div>
 
           <div className="text-sm text-gray-500">
-            © 2026 SimpleBank
+            © {new Date().getFullYear()} SimpleBank
           </div>
         </div>
       </div>
